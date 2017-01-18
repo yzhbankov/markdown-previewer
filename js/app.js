7e14f64ead3e3481ab1cf35b7aac8005e504aff9
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-18 22:42:13 +0200

    add state checking

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Iaroslav Zhbankov on 16.01.2017.
  */
-const text = '#Heading\n=======\n\nSub-heading\n-----------\n' +
+const text = '# Heading\n=======\n\nSub-heading\n-----------\n' +
     ' \n### Another deeper heading\n \nParagraphs are separated\n' +
     'by a blank line.\n\nLeave 2 spaces at the end of a line to do a  \n' +
     'line break\n\nText attributes *italic*, **bold**, \n' +
@@ -10,30 +10,32 @@ const text = '#Heading\n=======\n\nSub-heading\n-----------\n' +
     '  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n' +
     ' *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
 
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: true,
-    pedantic: true,
-    sanitize: true,
-    smartLists: true,
-    smartypants: true
-});
+var Markdown = React.createClass ({
+    getInitialState: function(){
+        return{
+            text:text
+        }
+    },
+    onChange: function(event){
 
-class Markdown extends React.Component {
-    render() {
+    var textarea = event.target;
+    this.setState({
+        text:textarea.value
+    });
+        console.log(textarea.value);
+},
+
+    render: function() {
         return (
             <div className='row'>
-                <div className='input col-md-6'><textarea type="text" className='frame'>
+                <div className='input col-md-6'><textarea onChange={this.onChange} type="text" className='frame'>
         {text}
     </textarea></div>
-                <div className='output col-md-6' dangerouslySetInnerHTML={{__html: marked(text)}}></div>
+                <div className='output col-md-6' dangerouslySetInnerHTML={{__html: marked(this.state.text)}}></div>
             </div>
         );
     }
-}
-
+});
 
 ReactDOM.render(
     <Markdown />,
